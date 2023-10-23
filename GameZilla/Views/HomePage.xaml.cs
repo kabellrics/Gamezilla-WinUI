4186ac203e2b13f6f461244773800ec2b67189e5
kabellrics <yflechel@gmail.com>
2023-10-23 18:33:21 +0200

    Read MergedDictionary & update gitignore

--- GameZilla/Views/HomePage.xaml.cs
+++ GameZilla/Views/HomePage.xaml.cs
@@ -20,32 +20,24 @@ public sealed partial class HomePage : Page
     }
     private async void LoadResourceDictionary(string styleKey)
     {
-        ResourceDictionary resourceDictionary = new ResourceDictionary();
-        if(await FileExistsAsync($"Styles/Skin/{styleKey}.xaml"))
+        ResourceDictionary resourceDictionary = GetResourceDictgionnary(styleKey);
+        if(resourceDictionary != null)
         {
-            resourceDictionary.Source = new Uri($"Styles/Skin/{styleKey}.xaml");
             // Assurez-vous de vider les anciens styles s'il y en a.
             if (this.Resources.MergedDictionaries.Count > 0)
             {
-                this.Resources.MergedDictionaries.Clear();
+                this.Resources.MergedDictionaries.Where(x => x is ResourceDictionary).Clear();
             }
 
             // Ajoutez le nouveau ResourceDictionary à la collection de ressources de la page.
             this.Resources.MergedDictionaries.Add(resourceDictionary);
         }
     }
-    private async Task<bool> FileExistsAsync(string filePath)
-    {
-        try
-        {
-            // Obtenez le fichier depuis l'emplacement spécifié.
-            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("/ms-appx:///" + filePath));
-            return true; // Le fichier existe.
-        }
-        catch
-        {
-            return false; // Le fichier n'existe pas.
-        }
-    }
 
+    private ResourceDictionary GetResourceDictgionnary(string styleKey)
+    {
+        var appResources = Application.Current.Resources;
+        var mergedDictionaries = appResources.MergedDictionaries.Where(x => x is ResourceDictionary);
+        return mergedDictionaries.FirstOrDefault(x => x.Source.ToString().Contains(styleKey));
+    }
 }
