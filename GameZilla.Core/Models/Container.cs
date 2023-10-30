e270723488e698b4c7873ece9c6ac4709cd59b7e
kabellrics <yflechel@gmail.com>
2023-10-30 18:20:55 +0100

    Start All Plateformes Pages

---
+++ GameZilla.Core/Models/Container.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameZilla.Core.Models;
+public class Container
+{
+    public string Id
+    {
+        get; set;
+    }
+    public string Name
+    {
+        get; set;
+    }
+    public string Fanart
+    {
+        get; set;
+    }
+    public string Logo
+    {
+        get; set;
+    }
+    public string ShowOrder
+    {
+        get; set;
+    }
+    public bool IsPlateforme
+    {
+        get; set;
+    }
+    public string IsActif
+    {
+        get; set;
+    }
+}
